f155da178670d273cca1792ebb25bc69985b9783
czhongx123 <792006914@qq.com>
2019-05-10 20:31:49 +0800

    uodate

--- hello-world/hello/src/App.vue
+++ hello-world/hello/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>我爱学习</h2>
+    <HelloWorld msg="Welcom to Your Vue.js App"/>
   </div>
 </template>
 
